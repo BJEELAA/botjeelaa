5fbdf62b79a2cbc7e9811a34462cead1c9304ce3
BJEELAA <belahoutman@gmail.com>
2021-03-24 12:18:03 +0100

    templatestring

--- index.js
+++ index.js
@@ -114,8 +114,9 @@ client.on("message", async message => {
         let rng = Math.random();
         let facts = ["Jerrie is een neushoorn"," Max is DE MAX","Karanbier houdt van pilsjes","Gunks is big chungus","Liere houdt van 'lire' (lezen)","Sandy ~~hates~~ loves sand","Max zijn laptop houdt van ontploffen","Lowiek is niet zo slim","Neushoorns zijn altijd Jerrie","Bela likes to be called C-la   -Lil G","Winston likes to **win** **ston**ks   -Lil G","haha programming go brrrrrr","Mario vindt Bowser gay, en zegt dit in een van de games","banana","haha stinky stinky"]
         let fact = facts[Math.floor(rng * facts.length)];
-        let aaa=Math.floor(rng * facts.length)+1
-        let print = "Fun Fact " + aaa + ": " + fact
+        let factNr=Math.floor(rng * facts.length)+1
+        let slash = "/"
+        let print = `Fun Fact ${factNr}${slash}${facts.length}: ${fact}`
         return message.channel.send(print);
         
     }
