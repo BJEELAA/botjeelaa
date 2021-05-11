4974f3367e4223c4eae8cc9ec4e12930185b9048
BJEELAA <belahoutman@gmail.com>
2021-05-11 15:38:08 +0200

    fixed rps

--- commands/rps.js
+++ commands/rps.js
@@ -1,26 +1,34 @@
 const discord = require("discord.js");
 
-module.exports.run = async(client, message, args, messageArray) =>{
+module.exports.run = async (client, message, args, messageArray) => {
 
-    var rng = Math.random();
-    var number = rng*3;
-    var c = number.ceil();
+    if (args.length != 0) {
 
-    if(computer == 1) computer = "rock";
-    if(computer == 2) computer = "paper";
-    if(computer == 2) computer = "scissors";
+        if (args[0].toLowerCase() == "rock" || args[0].toLowerCase() == "paper" || args[0].toLowerCase() == "scissors") {
 
-    var p = args[0].toLowerCase();
+            var rng = Math.random();
+            var number = rng * 3;
+            var c = Math.ceil(number);
 
-    if(c == "rock" && p == "rock") return message.channel.send("We both picked **rock**, so it's a tie");
-    if(c == "paper" && p == "paper") return message.channel.send("We both picked **paper**, so it's a tie");
-    if(c == "scissors" && p == "scissors") return message.channel.send("We both picked **scissors**, so it's a tie");
-    if(c == "rock" && p == "paper") return message.channel.send("I picked **rock** and you picked **paper**, so you win :(");
-    if(c == "rock" && p == "scissors") return message.channel.send("I picked **rock** and you picked **scissors**, so I win :D");
-    if(c == "paper" && p == "rock") return message.channel.send("I picked **paper** and you picked **rock**, so I win :D");
-    if(c == "paper" && p == "scissors") return message.channel.send("I picked **paper** and you picked **scissors**, so you win :(");
-    if(c == "scissors" && p == "rock") return message.channel.send("I picked **scissors** and you picked **rock**, so you win :(");
-    if(c == "scissors" && p == "paper") return message.channel.send("I picked **scissors** and you picked **paper**, so I win :D");
+            if (c == 1) c = "rock";
+            if (c == 2) c = "paper";
+            if (c == 3) c = "scissors";
+
+            var p = args[0].toLowerCase();
+
+            if (c == "rock" && p == "rock") return message.channel.send("We both picked **rock**, so it's a tie");
+            if (c == "paper" && p == "paper") return message.channel.send("We both picked **paper**, so it's a tie");
+            if (c == "scissors" && p == "scissors") return message.channel.send("We both picked **scissors**, so it's a tie");
+            if (c == "rock" && p == "paper") return message.channel.send("I picked **rock** and you picked **paper**, so you win :(");
+            if (c == "rock" && p == "scissors") return message.channel.send("I picked **rock** and you picked **scissors**, so I win :D");
+            if (c == "paper" && p == "rock") return message.channel.send("I picked **paper** and you picked **rock**, so I win :D");
+            if (c == "paper" && p == "scissors") return message.channel.send("I picked **paper** and you picked **scissors**, so you win :(");
+            if (c == "scissors" && p == "rock") return message.channel.send("I picked **scissors** and you picked **rock**, so you win :(");
+            if (c == "scissors" && p == "paper") return message.channel.send("I picked **scissors** and you picked **paper**, so I win :D");
+
+        }else return message.channel.send("Please use: !rps rock/paper/scissors");
+
+    }else return message.channel.send("Please use: !rps rock/paper/scissors");
 
 }
 
