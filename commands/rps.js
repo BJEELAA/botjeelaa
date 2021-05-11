b2386b3158a2fcff4d82c68eb19e3cc86a1c0a7b
BJEELAA <belahoutman@gmail.com>
2021-05-11 11:14:17 +0200

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -2,6 +2,14 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args, messageArray) =>{
 
+    if(args.length() != 0){
+        let rng = Math.random();
+        let a = rng*3
+        let number = a.ceil();
+        if(number == 1){
+            print ()
+        }
+    }else return message.channel.send("Usage: !rps [rock, paper, scissors]")
     
 
 }
