019acec102c85fb7f5d663581520add9c92c3f8d
BJEELAA <belahoutman@gmail.com>
2021-05-11 15:27:47 +0200

    rps & leerkracht

--- commands/leerkracht.js
+++ commands/leerkracht.js
@@ -13,13 +13,13 @@ module.exports.run = async(client, message, args, messageArray) =>{
     if(args.length === 0) return message.channel.send("Add a teacher: !leerkracht [last name teacher]");
 
     lkr("Bracke", "Mr. Bracke is onze informatica leerkracht.\n Hij leert ons Excel.");
-    lkr("Van Cauter", "Mvr. Van Cauter is onze IW leerkracht, ze leert ons erg weinig bij.");
-    lkr("De Lauw", "Mr. De Lauw, beter bekend als De Lauwe Worst, is onze fysica/wiskunde leerkracht.");
+    lkr("VanCauter", "Mvr. Van Cauter is onze IW leerkracht, ze leert ons erg weinig bij.");
+    lkr("DeLauw", "Mr. De Lauw, beter bekend als De Lauwe Worst, is onze fysica/wiskunde leerkracht.");
     lkr("Kint", "Mvr. Kint is onze erg saaie geschiedenis leerkracht. Door zwangerschap is ze erg lang afwezig geweest, dus dat is leuk.");
-    lkr("Van der Eecken", "KVdE is onze BIO/Chemie leerkracht en onze klastitularis. Ze probeert grappig te zijn maar ze is gewoon erg cringe.");
+    lkr("VanderEecken", "KVdE is onze BIO/Chemie leerkracht en onze klastitularis. Ze probeert grappig te zijn maar ze is gewoon erg cringe.");
     lkr("Deproost", "Mvr. Deproost is een stereotype boomer, en dus ook onze godsdienst leerkracht. We praten meer over politiek dan over godsdiesnt.");
     lkr("Neckebroeck", "Nekkie is onze leerkracht Frans, maar hij geeft les over van alles en nog wat. Hij is erg ambitieus en geeft graag buizen aan meisjes genaamd Margaux.");
-    lkr("Van Eck", "Mr. van Eck is onze leerkracht Engels. Hij speelt games en geeft extra opdrachten aan de goede werkers.");
+    lkr("VanEck", "Mr. van Eck is onze leerkracht Engels. Hij speelt games en geeft extra opdrachten aan de goede werkers.");
     lkr("Renard", "Mr. Renard is onze altijd-te-laat leerkracht Nederlands. Zoals ik zal zei, is hij al-tijd te laat. Dat is dan blijkbaar onze schuld dus gaat hij dan 10 minuten langer door.");
 
 }
--- commands/rps.js
+++ commands/rps.js
@@ -2,15 +2,25 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args, messageArray) =>{
 
-    if(args.length() != 0){
-        let rng = Math.random();
-        let a = rng*3
-        let number = a.ceil();
-        if(number == 1){
-            print ()
-        }
-    }else return message.channel.send("Usage: !rps [rock, paper, scissors]")
-    
+    var rng = Math.random();
+    var number = rng*3;
+    var c = number.ceil();
+
+    if(computer == 1) computer = "rock";
+    if(computer == 2) computer = "paper";
+    if(computer == 2) computer = "scissors";
+
+    var p = args[0].toLowerCase();
+
+    if(c == "rock" && p == "rock") return message.channel.send("We both picked **rock**, so it's a tie");
+    if(c == "paper" && p == "paper") return message.channel.send("We both picked **paper**, so it's a tie");
+    if(c == "scissors" && p == "scissors") return message.channel.send("We both picked **scissors**, so it's a tie");
+    if(c == "rock" && p == "paper") return message.channel.send("I picked **rock** and you picked **paper**, so you win :(");
+    if(c == "rock" && p == "scissors") return message.channel.send("I picked **rock** and you picked **scissors**, so I win :D");
+    if(c == "paper" && p == "rock") return message.channel.send("I picked **paper** and you picked **rock**, so I win :D");
+    if(c == "paper" && p == "scissors") return message.channel.send("I picked **paper** and you picked **scissors**, so you win :(");
+    if(c == "scissors" && p == "rock") return message.channel.send("I picked **scissors** and you picked **rock**, so you win :(");
+    if(c == "scissors" && p == "paper") return message.channel.send("I picked **scissors** and you picked **paper**, so I win :D");
 
 }
 
