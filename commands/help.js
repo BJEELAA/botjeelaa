bf982976da527efadfc1bec957f5bd7d1c1492b0
BJEELAA <belahoutman@gmail.com>
2021-05-14 13:08:12 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async(client, message, args, messageArray, prefix) =>{
 
     try{
 
-        var helpMsg = `✨**BOTJEELAA**✨\n\n**__Commands__**\n${prefix}botstatus - Gives info about the bot\n${prefix}cat - Gives a funny/cute cat picture\n${prefix}dm <id> <message> - DM a certain message to someone\n${prefix}funfact [nr] - Tells user a random fun fact\n${prefix}help - Sends this message\n${prefix}leerkracht <last name> - Gives info about one of my teachers\n${prefix}lenny - Sends a lenny face\n${prefix}rps <rock/paper/scissors> - Play rock paper scissors with bot\n${prefix}kick <@user> <reason> - Kick mentioned user\n${prefix}ban <@user> <reason> - Ban mentioned user\n\n**__Features__**\nWhen the bot receives a DM, it automatically logs the DM in channel #logjeelaa`;
+        var helpMsg = `✨**BOTJEELAA**✨\n\n**__Commands__**\n${prefix}botstatus - Gives info about the bot\n${prefix}cat - Gives a funny/cute cat picture\n${prefix}dm <id> <message> - DM a certain message to someone\n${prefix}funfact [nr] - Tells user a random fun fact\n${prefix}help - Sends this message\n${prefix}leerkracht <last name> - Gives info about one of my teachers\n${prefix}lenny - Sends a lenny face\n${prefix}rps <rock/paper/scissors> - Play rock paper scissors with bot\n${prefix}kick <@user> <reason> - Kick mentioned user\n${prefix}ban <@user> <reason> - Ban mentioned user\n${prefix}clear <amount> - Clear X amount of messages (max 100, max 2 weeks old)\n\n**__Features__**\nWhen the bot receives a DM, it automatically logs the DM in channel #logjeelaa`;
         message.author.send(helpMsg);
 
         message.channel.send("Sent the help message to your DMs");
