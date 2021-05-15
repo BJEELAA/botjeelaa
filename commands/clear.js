c1b63c15af2cf22d05e0c1391ee4788be39b5f8a
BJEELAA <belahoutman@gmail.com>
2021-05-15 20:09:41 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,20 +3,32 @@ const fs = require("fs");
 
 module.exports.run = async (client, message, args, messageArray, prefix) => {
 
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you don't have the necessary permissions to do this.");
+    if (message.deletable) {
+        message.delete();
+    }
+
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) return message.channel.send("That's either not a a number, or less than or equal to 0")
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, but you don't have the necessary permissions to do this.");
 
     if (!args[0]) return message.channel.send(`Usage: ${prefix}clear <amount>`);
 
-    if (Number.isInteger(parseInt(args[0]))) {
 
-        var amount = parseInt(args[0]) + 1;
-        message.channel.bulkDelete(amount).then(() => {
-            if (args[0] <= 0) message.channel.send("Can't really delete anything under 1 message, can I?").then(msg => msg.delete({ timeout: 3000 }));
-            if (args[0] == 1) message.channel.send("Deleted 1 message").then(msg => msg.delete({ timeout: 3000 }));
-            if (args[0] >= 2) message.channel.send(`Deleted ${args[0]} messages`).then(msg => msg.delete({ timeout: 3000 }));
-        })
 
-    } else return message.channel.send(`Usage: ${prefix}clear <amount> (Amount has to be a number)`);
+    let deleteAmount;
+
+    if (parseInt(args[0]) > 100) {
+        deleteAmount = 100;
+    } else {
+        deleteAmount = parseInt(args[0]);
+    }
+
+    message.channel.bulkDelete(deleteAmount, true)
+        .then(deleted => message.channel.send(`Cleared ${deleted.size} messages`))
+        .catch(err => message.reply(`Something went wrong... ${err}`))
+        .then(msg => msg.delete({ timeout: 3000 }));
+
+
 
 }
 
