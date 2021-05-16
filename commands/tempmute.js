2f1d4615cc4745e7cf4f11214ba8571cb74466ed
BJEELAA <belahoutman@gmail.com>
2021-05-16 16:13:10 +0200

    ms + tempmute

---
+++ commands/tempmute.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args, messageArray) =>{
+
+    message.channel.send("epic");
+
+}
+
+module.exports.help = {
+
+    name: "tempmute"
+
+}
