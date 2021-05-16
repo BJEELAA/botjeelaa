c872dbbad517eddc066e26c328a9894d30938929
BJEELAA <belahoutman@gmail.com>
2021-05-16 20:55:33 +0200

    announce cmd

---
+++ commands/announce.js
@@ -0,0 +1,52 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args, messageArray, prefix) => {
+
+    // _announce title | message | colour | channel
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You don't have the permissions to do this");
+
+    var seperator = "|";
+
+    var usage = `Usage: ${prefix}announce **title** ${seperator} **message** ${seperator} **colour** ${seperator} **channel**`;
+
+    if (!args[0]) return message.channel.send(usage)
+
+    var argsList = args.join(` `).split(seperator);
+
+    if (argsList[1] === undefined) argsList[1] = "No message provided";
+    if (argsList[2] === undefined) argsList[2] = "#0000ff";
+    if (argsList[3] === undefined) argsList[3] = "announcements";
+
+    var options = {
+
+        title: argsList[0].trim(),
+        message: argsList[1].trim(),
+        colour: argsList[2].trim(),
+        channel: argsList[3].trim()
+
+    }
+
+    var annEmbed = new discord.MessageEmbed()
+        .setTitle(options.title)
+        .setColor(options.colour)
+        .setDescription(`Message from ${message.author}
+        
+        ${options.message}`);
+    
+    var annChannel = client.channels.cache.find(channel => channel.name === options.channel);
+
+    var annPing = message.guild.roles.cache.find(role => role.name === "Announcement Ping")
+
+    if(!annChannel) return message.channel.send("Channel couldn't be found");
+
+    annChannel.send(`<@&${annPing.id}>`);
+    annChannel.send(annEmbed);
+
+}
+
+module.exports.help = {
+
+    name: "announce"
+
+}
