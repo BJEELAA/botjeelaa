1ab445be1ee1c8a25d7fcae2028204c5ebef81f3
BJEELAA <belahoutman@gmail.com>
2021-05-16 16:27:50 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,8 +1,17 @@
 const discord = require("discord.js");
+const ms = require("ms");
 
 module.exports.run = async(client, message, args, messageArray) =>{
 
-    message.channel.send("epic");
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Sorry, but you don't have the correct permissions to do this");
+    if (!message.mentions.members.first()) return message.reply(`Usage: ${prefix}mute <@person> <time>`);
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.reply("Seems like I don't have the necessary permissions to do this");
+
+    var muteUser = message.mentions.members.first();
+
+    if (!muteUser) return message.channel.send("Couldn't find user");
+
+    if (muteUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Can't mute moderators");
 
 }
 
