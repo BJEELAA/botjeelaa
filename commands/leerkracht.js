89c09290cc06e15ccad2bd6f307918a97b9e9f22
BJEELAA <belahoutman@gmail.com>
2021-06-03 15:48:17 +0200

    Update leerkracht.js

--- commands/leerkracht.js
+++ commands/leerkracht.js
@@ -17,7 +17,7 @@ module.exports.run = async(client, message, args, messageArray, prefix) =>{
     lkr("DeLauw", "Mr. De Lauw, beter bekend als De Lauwe Worst, is onze fysica/wiskunde leerkracht.");
     lkr("Kint", "Mvr. Kint is onze erg saaie geschiedenis leerkracht. Door zwangerschap is ze erg lang afwezig geweest, dus dat is leuk.");
     lkr("VanderEecken", "KVdE is onze BIO/Chemie leerkracht en onze klastitularis. Ze probeert grappig te zijn maar ze is gewoon erg cringe.");
-    lkr("Deproost", "Mvr. Deproost is een stereotype boomer, en dus ook onze godsdienst leerkracht. We praten meer over politiek dan over godsdiesnt.");
+    lkr("Deproost", "Mvr. Deproost is een stereotype boomer, en dus ook onze godsdienst leerkracht. We praten meer over politiek, s*x en verkrachting dan over godsdiesnt.");
     lkr("Neckebroeck", "Nekkie is onze leerkracht Frans, maar hij geeft les over van alles en nog wat. Hij is erg ambitieus en geeft graag buizen aan meisjes genaamd Margaux.");
     lkr("VanEck", "Mr. van Eck is onze leerkracht Engels. Hij speelt games en geeft extra opdrachten aan de goede werkers.");
     lkr("Renard", "Mr. Renard is onze altijd-te-laat leerkracht Nederlands. Zoals ik zal zei, is hij al-tijd te laat. Dat is dan blijkbaar onze schuld dus gaat hij dan 10 minuten langer door.");
